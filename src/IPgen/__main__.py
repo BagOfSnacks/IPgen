0cbe83ee1497da71a8e16edf6064311ddf780e9a
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-02-23 12:01:27 +0100

    Improvements

--- src/IPgen/__main__.py
+++ src/IPgen/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from random import sample
 from typing import Optional, Sequence
 
--- tests/test_SavingToFile.py
+++ tests/test_SavingToFile.py
@@ -1,2 +1,18 @@
+import os
+
+from IPgen.__main__ import random_IPv4_Adress
+from IPgen.util import is_IPv4_adress
 from IPgen.FileSaver import TxtSaver, JSONSaver
 from IPgen.const import PATH
+
+def test_file_is_saved():
+    addresses = [random_IPv4_Adress() for _ in range(0,3)]
+    saver = TxtSaver(contents=addresses, filename="example")
+    saver.save_to_file()
+    assert saver.filename + saver.extension in os.listdir()
+
+def test_contents_are_ipv4_addresses():
+    with open("example.txt", "r") as f:
+        content = f.readlines()
+        valid = map(is_IPv4_adress, content)
+    assert all(valid) is True
