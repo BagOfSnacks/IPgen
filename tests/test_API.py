dd0546883c2a74058edbaf1a6ec7992107aadb98
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-03-26 21:54:49 +0200

    Updated API tests to skip if IP address is private

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,8 +1,11 @@
 import pytest
+import ipaddress
+
 from urllib.request import urlopen
 
 from IPgen.ipinfo import APIRequest
 from IPgen.generators import random_ipv4_address, random_ipv6_address
+from IPgen.util import is_ipv4_address, is_ipv6_address
 
 
 try:
@@ -11,14 +14,38 @@ except:
     pytest.skip(reason="No internet connection, API calls cannot be made", allow_module_level=True)
 
 
+def is_private_ip(ip_str: str):
+    if is_ipv4_address(ip_str):
+        ip = ipaddress.IPv4Address(ip_str)
+        return ip.is_private
+
+    if is_ipv6_address(ip_str):
+        ip = ipaddress.IPv6Address(ip_str)
+        return ip.is_private
+
+
+def skip_if_private_ip(ip_str: str):
+    if is_private_ip(ip_str):
+        pytest.skip(reason="Skipping test for private IP address")
+
+
 @pytest.fixture
 def random_ipv4() -> str:
-    return str(random_ipv4_address())
+    address = str(random_ipv4_address())
+    skip_if_private_ip(address)
+    return address
 
 
 @pytest.fixture
 def random_ipv6() -> str:
-    return str(random_ipv6_address())
+    address = str(random_ipv6_address())
+    skip_if_private_ip(address)
+    return address
+
+
+@pytest.fixture
+def private_ipv4() -> str:
+    return "192.168.1.1"
 
 
 @pytest.fixture
@@ -42,7 +69,13 @@ def test_API_call_successfull_v4(API_call_v4):
     assert API_call_v4['status'] == 'success'
 
 
+# This test should be considered as 'passed' if it successfully skips
+def test_API_call_skip_on_private_ip(private_ipv4):
+    skip_if_private_ip(private_ipv4)
+
+
 def test_API_call_successfull_v6(API_call_v6):
+
     assert API_call_v6['status'] == 'success'
 
 
